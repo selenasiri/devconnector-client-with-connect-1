9b539517fc23669d9ba8364d6e0ded55c8322faf
Selena Siri <selenasiri@gmail.com>
2020-10-24 09:59:35 -0400

    dashboard useSelector

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,23 +1,20 @@
 import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import DashboardActions from './DashboardActions';
 import Experience from './Experience';
 import Education from './Education';
 import { getCurrentProfile, deleteAccount } from '../../actions/profile';
 
-const Dashboard = (props) => {
-  const {
-    getCurrentProfile,
-    deleteAccount,
-    auth: { user },
-    profile: { profile }
-  } = props;
+const Dashboard = () => {
+  const { user } = useSelector(state => state.auth);
+  const { profile } = useSelector(state => state.profile); 
+
+  const dispatch = useDispatch()
 
   useEffect(() => {
-    getCurrentProfile();
-  }, [getCurrentProfile]);
+    dispatch(getCurrentProfile());
+  }, [dispatch]);
 
   return (
     <>
@@ -52,18 +49,4 @@ const Dashboard = (props) => {
   );
 };
 
-Dashboard.propTypes = {
-  getCurrentProfile: PropTypes.func.isRequired,
-  deleteAccount: PropTypes.func.isRequired,
-  auth: PropTypes.object.isRequired,
-  profile: PropTypes.object.isRequired
-};
-
-const mapStateToProps = (state) => ({
-  auth: state.auth,
-  profile: state.profile
-});
-
-export default connect(mapStateToProps, { getCurrentProfile, deleteAccount })(
-  Dashboard
-);
+export default Dashboard;
